ac3b12375d0803e043e1288a4b9a2c2143699e3c
Subaash1706 <subaashbala@gmail.com>
2023-11-02 13:56:53 +0530

    update expensehistory

--- pages/expenseHistory.js
+++ pages/expenseHistory.js
@@ -4,7 +4,7 @@ const body = document.querySelector('body');
 const lsObjArray = [];
 const recordYears = new Set(); // DECLARED A SET TO FILTER OUT THE UNIQUE VALUES OF YEARS PRESENT IN THE STORED ENTRY
 const paraTag = document.createElement('div');
-paraTag.innerHTML = 'Add some data to view yearwise Expense/Income history, dat unavailable'
+paraTag.innerHTML = 'Add some data to view yearwise Expense/Income history'
 function fetchLocalStorageData(){ // FETCHES LS DATA AS USUAL
     try{
         const lsArray = localStorage.getItem('expenseArray')
@@ -72,7 +72,7 @@ function renderYearCards(recordYearsArray){
         cardContainer.appendChild(cardBody)
         monthContainer.appendChild(cardContainer);
         cardContainer.style.backgroundImage = `url(../images/vectors/bg-${Math.floor(Math.random() * 6)}.jpg)`;
-body.appendChild(cardContainer)
+// body.appendChild(cardContainer)
     })    
     }
     else{
